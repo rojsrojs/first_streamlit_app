bff5bf47bef8f554fdf0768e8a80e25bfe1c9d36
rojsrojs <daniel.odin@gmail.com>
2022-10-31 10:48:47 +0100

    close snowflake connection and add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,12 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 
-if streamlit.button('Get Fruit Load List'):
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
@@ -62,6 +65,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 streamlit.stop()
